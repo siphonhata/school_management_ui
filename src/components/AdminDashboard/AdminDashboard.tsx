5838d5a4988d21660dcd588517ea8ef1bfbd5e4c
Dineo Mathibela <dineomathibela161@gmail.com>
2024-05-18 22:06:01 +0200

    fix:

--- src/components/AdminDashboard/AdminDashboard.tsx
+++ src/components/AdminDashboard/AdminDashboard.tsx
@@ -4,7 +4,7 @@ import { ProfilePage, ProfileView } from "../Profile";
 
 const navItems = [
     { path: 'dashboard', name: 'Dashboard', icon: <HomeIcon className="h-5 w-5" />, Component: <h1>Home Component</h1> },
-    { path: 'profile', name: 'Profile', icon: <CreditCardIcon className="h-5 w-5" />, Component: <><ProfileView /> </> },
+    { path: 'profile', name: 'Profile', icon: <CreditCardIcon className="h-5 w-5" />, Component: <><ProfilePage /> </> },
     { path: 'students', name: 'Students', icon: <UserGroupIcon className="h-5 w-5" />, Component: <h1>Home Component</h1> },
     { path: 'teachers', name: 'Teachers', icon: <UserIcon className="h-5 w-5" />, Component: <h1>Home Component</h1> },
     { path: 'attendance', name: 'Attendance', icon: <ClipboardIcon className="h-5 w-5" />, Component: <h1>Home Component</h1> },
