f26ff2b11c596af610df6813b6e83b5592d21dd6
Dineo Mathibela <dineomathibela161@gmail.com>
2024-05-18 21:00:43 +0200

    add: profile on nav

--- src/components/Dashboard/DashboardRoutes.tsx
+++ src/components/Dashboard/DashboardRoutes.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import { ProfilePage } from '../Profile';
 export const Dashboard = ({ navItems }: any) => {
+    console.log("navItems", navItems)
     return (
         <Routes>
             {navItems.map((item: any) => (
-                <Route key={item.path} path={item.path} element={<ProfilePage />} />
+                <Route key={item.path} path={item.path} element={item.Component} />
             ))}
         </Routes>
     );
