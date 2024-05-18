7045e57bb7430eedec4733fcd857806a9ac88c8e
Dineo Mathibela <dineomathibela161@gmail.com>
2024-05-18 22:03:57 +0200

    trial Profile view

--- src/components/AdminDashboard/AdminDashboard.tsx
+++ src/components/AdminDashboard/AdminDashboard.tsx
@@ -1,10 +1,10 @@
 import { HomeIcon, UserGroupIcon, UserIcon, BookOpenIcon, ClipboardIcon, AcademicCapIcon, CreditCardIcon } from "@heroicons/react/24/solid";
 import { Dashboard, DashboardLayout } from "../Dashboard"
-import { ProfilePage } from "../Profile";
+import { ProfilePage, ProfileView } from "../Profile";
 
 const navItems = [
     { path: 'dashboard', name: 'Dashboard', icon: <HomeIcon className="h-5 w-5" />, Component: <h1>Home Component</h1> },
-    { path: 'profile', name: 'Profile', icon: <CreditCardIcon className="h-5 w-5" />, Component: <><ProfilePage /> </> },
+    { path: 'profile', name: 'Profile', icon: <CreditCardIcon className="h-5 w-5" />, Component: <><ProfileView /> </> },
     { path: 'students', name: 'Students', icon: <UserGroupIcon className="h-5 w-5" />, Component: <h1>Home Component</h1> },
     { path: 'teachers', name: 'Teachers', icon: <UserIcon className="h-5 w-5" />, Component: <h1>Home Component</h1> },
     { path: 'attendance', name: 'Attendance', icon: <ClipboardIcon className="h-5 w-5" />, Component: <h1>Home Component</h1> },
