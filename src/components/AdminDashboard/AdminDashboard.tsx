c3a92b6404d5ff8c326f272cdfb7d79b458803f3
Dineo Mathibela <dineomathibela161@gmail.com>
2024-05-22 23:39:08 +0200

    add: Teacher Placeholder

--- src/components/AdminDashboard/AdminDashboard.tsx
+++ src/components/AdminDashboard/AdminDashboard.tsx
@@ -2,12 +2,13 @@ import { HomeIcon, UserGroupIcon, UserIcon, BookOpenIcon, ClipboardIcon, Academi
 import { Dashboard, DashboardLayout } from "../Dashboard"
 import { ProfilePage } from "../Profile";
 import { StudentsComponent } from "./Students";
+import { TeachersComponent } from "./Teachers";
 
 const navItems = [
     { path: 'dashboard', name: 'Dashboard', icon: <HomeIcon className="h-5 w-5" />, Component: <h1>Home Component</h1> },
     { path: 'profile', name: 'Profile', icon: <CreditCardIcon className="h-5 w-5" />, Component: <><ProfilePage /> </> },
     { path: 'students', name: 'Students', icon: <UserGroupIcon className="h-5 w-5" />, Component: <><StudentsComponent /></> },
-    { path: 'teachers', name: 'Teachers', icon: <UserIcon className="h-5 w-5" />, Component: <h1>Home Component</h1> },
+    { path: 'teachers', name: 'Teachers', icon: <UserIcon className="h-5 w-5" />, Component: <><TeachersComponent /></> },
     { path: 'attendance', name: 'Attendance', icon: <ClipboardIcon className="h-5 w-5" />, Component: <h1>Home Component</h1> },
     { path: 'courses', name: 'Courses', icon: <BookOpenIcon className="h-5 w-5" />, Component: <h1>Home Component</h1> },
     { path: 'exams', name: 'Exams', icon: <AcademicCapIcon className="h-5 w-5" />, Component: <h1>Home Component</h1> },
---
+++ src/components/AdminDashboard/Teachers.tsx
@@ -0,0 +1,7 @@
+import { PlaceHolder } from "../Common"
+
+export const TeachersComponent = () => {
+    return (
+        <PlaceHolder imageSrc={'/addteachers.png'} description={"A list of Teachers"} />
+    )
+}
