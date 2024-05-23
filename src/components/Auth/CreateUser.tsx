14ce0fde8524ca7f2997bb5556b97665ccdc8f5c
siphonhata <siphonhata@gmail.com>
2024-05-23 02:26:27 +0200

    feat:create user

--- src/components/Auth/CreateUser.tsx
+++ src/components/Auth/CreateUser.tsx
@@ -1,142 +1,152 @@
-import axios from 'axios';
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { api_url } from '../../App';
+import axios from "axios";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { api_url } from "../../App";
 
 export const SignUp: React.FC = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
-    const [errorMessage, setErrorMessage] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const nav = useNavigate();
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      //@ts-ignore
+      const response = await axios.post(`${api_url}/create_user`, {
+        email,
+        password,
+        first_name: firstName,
+        last_name: lastName,
+      });
 
-    const handleSubmit = async (e: React.FormEvent) => {
-        e.preventDefault();
+      if (response?.data.success) {
+        nav("/signin");
+      }
 
-        console.log("trial: ", email,
-            password,
-            firstName,
-            lastName,
-        )
-        try {
-            //@ts-ignore
-            const response = await axios.post(`${api_url}/create_user`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    email,
-                    password,
-                    first_name: firstName,
-                    last_name: lastName,
-                }),
-            });
+      setEmail("");
+      setPassword("");
+      setFirstName("");
+      setLastName("");
+      setErrorMessage("");
+    } catch (error) {
+      setErrorMessage("Sign-up failed. Please try again.");
+    }
+  };
 
-            // if (!response.ok) {
-            //     throw new Error('Sign-up failed');
-            // }
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      {/* Overlay */}
+      <div className="absolute inset-0 bg-black opacity-50 z-10"></div>
 
-            // Reset form fields
-            setEmail('');
-            setPassword('');
-            setFirstName('');
-            setLastName('');
-            setErrorMessage('');
+      {/* Sign Up Form */}
+      <div className="bg-white p-6 rounded-lg shadow-lg z-20 w-full max-w-md relative">
+        <h1 className="text-2xl font-bold mb-4">Sign Up</h1>
+        <form onSubmit={handleSubmit}>
+          {/* First Name */}
+          <div className="mb-4">
+            <label
+              htmlFor="firstName"
+              className="block text-sm font-medium text-gray-700"
+            >
+              First Name
+              <span className="m-1 text-md font-bold text-red-400">*</span>
+            </label>
+            <input
+              type="text"
+              id="firstName"
+              name="firstName"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+              required
+              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+            />
+          </div>
 
-            console.log("Successfully signed up")
-        } catch (error) {
-            setErrorMessage('Sign-up failed. Please try again.');
-        }
-    };
+          {/* Last Name */}
+          <div className="mb-4">
+            <label
+              htmlFor="lastName"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Last Name
+              <span className="m-1 text-md font-bold text-red-400">*</span>
+            </label>
+            <input
+              type="text"
+              id="lastName"
+              name="lastName"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+              required
+              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+            />
+          </div>
 
+          {/* Email */}
+          <div className="mb-4">
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Email<span className="m-1 text-md font-bold text-red-400">*</span>
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+            />
+          </div>
 
-    return (
-        <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-            {/* Overlay */}
-            <div className="absolute inset-0 bg-black opacity-50 z-10"></div>
+          {/* Password */}
+          <div className="mb-4">
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Password
+              <span className="m-1 text-md font-bold text-red-400">*</span>
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+            />
+          </div>
 
-            {/* Sign Up Form */}
-            <div className="bg-white p-6 rounded-lg shadow-lg z-20 w-full max-w-md relative">
-                <h1 className="text-2xl font-bold mb-4">Sign Up</h1>
-                <form onSubmit={handleSubmit}>
+          {/* Submit Button */}
+          <div className="mb-4">
+            <button
+              type="submit"
+              className="w-full hover:bg-blue-500 text-white px-3 py-2 rounded-md bg-blue-700"
+            >
+              Sign Up
+            </button>
+          </div>
 
+          {/* Error Message */}
+          {errorMessage && (
+            <p className="text-red-500 text-sm">{errorMessage}</p>
+          )}
+        </form>
 
-                    {/* First Name */}
-                    <div className="mb-4">
-                        <label htmlFor="firstName" className="block text-sm font-medium text-gray-700">First Name<span className='m-1 text-md font-bold text-red-400'>*</span></label>
-                        <input
-                            type="text"
-                            id="firstName"
-                            name="firstName"
-                            value={firstName}
-                            onChange={(e) => setFirstName(e.target.value)}
-                            required
-                            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-                        />
-                    </div>
-
-                    {/* Last Name */}
-                    <div className="mb-4">
-                        <label htmlFor="lastName" className="block text-sm font-medium text-gray-700">Last Name<span className='m-1 text-md font-bold text-red-400'>*</span></label>
-                        <input
-                            type="text"
-                            id="lastName"
-                            name="lastName"
-                            value={lastName}
-                            onChange={(e) => setLastName(e.target.value)}
-                            required
-                            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-                        />
-                    </div>
-
-
-                    {/* Email */}
-                    <div className="mb-4">
-                        <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email<span className='m-1 text-md font-bold text-red-400'>*</span></label>
-                        <input
-                            type="email"
-                            id="email"
-                            name="email"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            required
-                            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-                        />
-                    </div>
-
-
-                    {/* Password */}
-                    <div className="mb-4">
-                        <label htmlFor="password" className="block text-sm font-medium text-gray-700">Password<span className='m-1 text-md font-bold text-red-400'>*</span></label>
-                        <input
-                            type="password"
-                            id="password"
-                            name="password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            required
-                            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-                        />
-                    </div>
-
-                    {/* Submit Button */}
-                    <div className="mb-4">
-                        <button type="submit" className="w-full hover:bg-blue-500 text-white px-3 py-2 rounded-md bg-blue-700">
-                            Sign Up
-                        </button>
-                    </div>
-
-                    {/* Error Message */}
-                    {errorMessage && <p className="text-red-500 text-sm">{errorMessage}</p>}
-                </form>
-
-                {/* Link to Login Page */}
-                <p className="mt-4 text-center text-sm text-gray-600">
-                    Already have an account? <Link to="/signin" className="text-blue-500 hover:underline">Login</Link>
-                </p>
-            </div>
-        </div>
-    );
+        {/* Link to Login Page */}
+        <p className="mt-4 text-center text-sm text-gray-600">
+          Already have an account?{" "}
+          <Link to="/signin" className="text-blue-500 hover:underline">
+            Login
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
 };
-
--- src/components/Auth/SignIn.tsx
+++ src/components/Auth/SignIn.tsx
@@ -1,37 +1,37 @@
-import axios from 'axios';
-import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import { api_url } from '../../App';
+import axios from "axios";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { api_url } from "../../App";
 
 export const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
-  const nav = useNavigate()
+  const nav = useNavigate();
   const handleSubmit = async (event: any) => {
     event.preventDefault();
 
-    // Simple validation (replace with your actual logic)
     if (!email || !password) {
-      setErrorMessage('Please fill in all fields');
+      setErrorMessage("Please fill in all fields");
       return;
     }
-    console.log(email, password)
     try {
       //@ts-ignore
-      const response = await axios.post(`${api_url}/login`, { email, password });
+      const response = await axios.post(`${api_url}/login`, {
+        email,
+        password,
+      });
       if (response?.data.success) {
         if (response.data.token) {
-          localStorage.setItem('_Ey_', response.data.token);
-          nav('/dashboard');
+          localStorage.setItem("_Ey_", response.data.token);
+          nav("/dashboard");
         }
       } else {
         setErrorMessage(response?.data.message);
       }
     } catch (error) {
-      console.error('Error logging in:', error);
-      setErrorMessage('An error occurred. Please try again.');
+      setErrorMessage("An error occurred. Please try again.");
     }
   };
 
@@ -45,7 +45,12 @@ export const Login = () => {
         <h1 className="text-2xl font-bold mb-4">Login</h1>
         <form onSubmit={handleSubmit}>
           <div className="mb-4">
-            <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email<span className='m-1 text-md font-bold text-red-400'>*</span></label>
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Email<span className="m-1 text-md font-bold text-red-400">*</span>
+            </label>
             <input
               type="email"
               id="email"
@@ -57,7 +62,13 @@ export const Login = () => {
             />
           </div>
           <div className="mb-4">
-            <label htmlFor="password" className="block text-sm font-medium text-gray-700">Password<span className='m-1 text-md font-bold text-red-400'>*</span></label>
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Password
+              <span className="m-1 text-md font-bold text-red-400">*</span>
+            </label>
             <input
               type="password"
               id="password"
@@ -69,18 +80,22 @@ export const Login = () => {
             />
           </div>
           <div className="mb-4">
-            <button type="submit" className="w-full hover:bg-blue-500 text-white px-3 py-2 rounded-md bg-blue-700">
+            <button
+              type="submit"
+              className="w-full hover:bg-blue-500 text-white px-3 py-2 rounded-md bg-blue-700"
+            >
               Login
             </button>
           </div>
         </form>
         {errorMessage && <p className="text-red-500 text-sm">{errorMessage}</p>}
         <p className="bottom-4 left-0 right-0 text-center text-sm text-gray-600">
-          Don't have an account? <Link to="/signup" className="text-blue-500 hover:underline">Sign Up</Link>
+          Don't have an account?{" "}
+          <Link to="/signup" className="text-blue-500 hover:underline">
+            Sign Up
+          </Link>
         </p>
       </div>
-
     </div>
   );
 };
-
