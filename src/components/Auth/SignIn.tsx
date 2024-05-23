e44b99d496eb3276e9ac6e36088e0e3e3016f3e6
siphonhata <siphonhata@gmail.com>
2024-05-23 02:50:18 +0200

    feat: signin-eye-visibility

--- src/components/Auth/SignIn.tsx
+++ src/components/Auth/SignIn.tsx
@@ -2,11 +2,14 @@ import axios from "axios";
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { api_url } from "../../App";
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
+
 
 export const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
+  const [passwordVisible, setPasswordVisible] = useState(false);
 
   const nav = useNavigate();
   const handleSubmit = async (event: any) => {
@@ -34,6 +37,11 @@ export const Login = () => {
       setErrorMessage("An error occurred. Please try again.");
     }
   };
+
+  const togglePasswordVisibility = () => {
+    setPasswordVisible(!passwordVisible);
+  };
+
 
   return (
     <div className="relative min-h-screen bg-gray-100 flex items-center justify-center">
@@ -69,8 +77,9 @@ export const Login = () => {
               Password
               <span className="m-1 text-md font-bold text-red-400">*</span>
             </label>
+            <div className="relative">
             <input
-              type="password"
+              type={passwordVisible ? "text" : "password"}
               id="password"
               name="password"
               value={password}
@@ -78,6 +87,13 @@ export const Login = () => {
               required
               className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
             />
+            <span
+                onClick={togglePasswordVisibility}
+                className="absolute inset-y-0 right-0 pr-3 flex items-center cursor-pointer"
+              >
+                {passwordVisible ? <FaEyeSlash /> : <FaEye />}
+            </span>
+          </div>
           </div>
           <div className="mb-4">
             <button
