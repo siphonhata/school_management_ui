4e14ed48b38108fac314f8b96d4e000c03c8bb80
siphonhata <siphonhata@gmail.com>
2024-05-24 23:13:35 +0200

    dashboard card updates

--- src/components/AdminDashboard/Home.tsx
+++ src/components/AdminDashboard/Home.tsx
@@ -1,21 +1,121 @@
-import { TotalParentsCard, TotalRevenueCard, TotalTeachersCard, TotalStudentsCard } from './Cards';
+import React, { useEffect, useState } from "react";
+import { api_url } from "../../App";
+import { FaUserGraduate, FaUsers, FaChalkboardTeacher, FaDollarSign   } from "react-icons/fa";
 
-export const HomeAdmin = () => {
+export const HomeAdmin: React.FC = () => {
+  const [stats, setStats] = useState({
+    totalStudents: 0,
+    totalTeachers: 0,
+    totalParents: 0,
+  });
+
+  useEffect(() => {
+    async function fetchStats() {
+      try {
+        const response = await fetch(`${api_url}/stats`);
+        const data = await response.json();
+        setStats(data);
+      } catch (error) {
+        console.error("Error fetching stats:", error);
+      }
+    }
+
+    fetchStats();
+  }, []);
+
   return (
-    <div className="flex justify-start items-start h-screen bg-gray-100 p-2">
-        <div className="mx-1"><TotalStudentsCard /></div>
-        <div className="mx-1"><TotalTeachersCard /></div>
-        <div className="mx-1"><TotalParentsCard /></div>
-        <div className="mx-1"><TotalRevenueCard /></div>
+    <div className="flex justify-between">
+      <div className="w-60 h-16 bg-white text-gray-400 rounded-lg flex flex-col-reverse justify-between items-center shadow-sm p-4 mt-3">
+        <div className="flex items-center mt-4">
+          <FaUserGraduate className="mr-3 h-6 w-6 text-gray-900" />
+          <div>
+            <div className="text-sm font-semibold text-gray-400">
+              Total Students
+            </div>
+            <div className="font-medium text-gray-900">
+              {stats.totalStudents}
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="w-60 h-16 bg-white text-gray-400 rounded-lg flex flex-col-reverse justify-between items-center shadow-sm p-4 mt-3">
+      <div className="flex items-center mt-4">
+          <FaChalkboardTeacher className="mr-3 h-6 w-6 text-gray-900" />
+          <div>
+            <div className="text-sm font-semibold text-gray-400">
+              Total Teachers
+            </div>
+            <div className="font-medium text-gray-900">
+              {stats.totalTeachers}
+            </div>
+          </div>
+        </div>
+      </div>{" "}
+      <div className="w-60 h-16 bg-white text-gray-400 rounded-lg flex flex-col-reverse justify-between items-center shadow-sm p-4 mt-3">
+      <div className="flex items-center mt-4">
+          <FaUsers className="mr-3 h-6 w-6 text-gray-900" />
+          <div>
+            <div className="text-sm font-semibold text-gray-400">
+              Total Parents
+            </div>
+            <div className="font-medium text-gray-900">
+              {stats.totalParents}
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="w-60 h-16 bg-white text-gray-400 rounded-lg flex flex-col-reverse justify-between items-center shadow-sm p-4 mt-3">
+      <div className="flex items-center mt-4">
+          <FaDollarSign className="mr-3 h-6 w-6 text-gray-900" />
+          <div>
+            <div className="text-sm font-semibold text-gray-400">
+              Total Revenue
+            </div>
+            <div className="font-medium text-gray-900">
+              {stats.totalStudents}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-    // <div className="w-48 h-24 bg-blue-500 text-white rounded-lg flex flex-col-reverse justify-between items-center shadow-lg p-4 mt-3"> {/* Added mt-12 for margin top */}
-    //   <div className="flex items-center text-xl font-semibold">
-    //     Total Students
-    //   </div>
-    //   <FaGraduationCap className="text-3xl font-bold text-black" /> {/* Adding the graduation cap icon */}
-    //   <div className="flex items-center justify-center text-3xl font-bold text-black">
-    //     123
-    //   </div>
-    // </div>
   );
 };
+
+// const TotalTeachersCard: React.FC = () => {
+//   return (
+//     <div className="w-48 h-16 bg-white text-gray-400 rounded-lg flex flex-col-reverse justify-between items-center shadow-sm p-4 mt-3">
+//       <div className="flex items-center justify-center font-medium text-gray-900">
+//         456
+//       </div>
+//       <div className="flex items-center text-sm font-semibold">
+//         Total Teachers
+//       </div>
+//     </div>
+//   );
+// }
+
+// const TotalParentsCard: React.FC = () => {
+//   return (
+//     // <div className="w-48 h-16 bg-white text-gray-400 rounded-lg flex flex-col-reverse justify-between items-center shadow-sm p-4 mt-3">
+//     //   <div className="flex items-center justify-center font-medium text-gray-900">
+//     //     789
+//     //   </div>
+//     //   <div className="flex items-center text-sm font-semibold">
+//     //     Total Parents
+//     //   </div>
+//     // </div>
+//   );
+// }
+
+// const TotalRevenueCard: React.FC = () => {
+//   return (
+//     // <div className="w-48 h-16 bg-white text-gray-400 rounded-lg flex flex-col-reverse justify-between items-center shadow-sm p-4 mt-3">
+//     //   <div className="flex items-center justify-center font-medium text-gray-900">
+//     //     101112
+//     //   </div>
+//     //   <div className="flex items-center text-sm font-semibold">
+//     //     Total Revenue
+//     //   </div>
+//     // </div>
+//   );
+// }
