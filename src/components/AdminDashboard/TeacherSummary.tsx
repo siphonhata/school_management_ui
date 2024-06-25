8b4ce086d21484f81199323a3706d61de6b1c587
Dineo Mathibela <dineomathibela161@gmail.com>
2024-06-25 22:00:00 +0200

    chore: teacher summary card

--- src/components/AdminDashboard/TeacherSummary.tsx
+++ src/components/AdminDashboard/TeacherSummary.tsx
@@ -4,13 +4,13 @@ import React from 'react';
 
 export const TeachersCard = ({ teachers }: any) => {
     return (
-        <div className="max-w-sm p-4 bg-white rounded-lg shadow-md">
-            <div className="flex justify-between items-center mb-4">
+        <div className="max-w-sm p-4 bg-gray-50 rounded-lg shadow-md">
+            <div className="flex justify-between items-center mb-4 text-gray-800">
                 <h2 className="text-xl font-bold">Teachers List</h2>
-                <a href="#" className="text-blue-500 hover:underline">See more</a>
+                <p className="hover:underline text-gray-300 hover:text-gray-500">See more</p>
             </div>
-            <div className="flex mb-4 -space-x-2">
-                {teachers.map((teacher: any, index: any) => (
+            <div className="flex mb-4 space-x-2">
+                {teachers.slice(0, 6).map((teacher: any, index: any) => (
                     <img
                         key={index}
                         src={teacher.avatar}
@@ -19,16 +19,18 @@ export const TeachersCard = ({ teachers }: any) => {
                     />
                 ))}
             </div>
-            <div className="flex">
+            <p className="text-md font-bold text-gray-800 m-2">Add Teachers</p>
+            <div className="flex border rounded-md bg-white overflow-hidden">
                 <input
                     type="email"
                     placeholder="Invite with email"
-                    className="w-full p-2 border rounded-l-lg focus:outline-none"
+                    className="w-full p-2 outline-none focus:outline-none"
                 />
-                <button className="p-2 bg-blue-500 text-white rounded-r-lg hover:bg-blue-600">
+                <button className="p-2 px-4 font-bold bg-gray-900 rounded-md text-white hover:bg-gray-800">
                     Invite
                 </button>
             </div>
+
         </div>
     );
 };
